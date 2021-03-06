6afd6a283a07123138857ea52f49bfbd79325ee5
alex <alexanderporokhnenko@microbilt.com>
2021-03-06 07:21:01 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PYTHONCMD=python3
+
+#Find main project direcory
+MAINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${MAINDIR}"
+MAINDIR=$PWD
+
+#Create new virtualenv
+$PYTHONCMD -m venv env
+source env/bin/activate
+$PYTHONCMD -m pip install scrapy
